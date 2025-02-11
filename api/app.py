57e1220503a67d01997ac754f45b17b0d8b5c970
Shivam Chikara <s.chikara6885@gmail.com>
2025-02-11 10:37:36 +0530

    change the port to 8080

--- api/app.py
+++ api/app.py
@@ -111,4 +111,4 @@ def predict_endpoint():
         return jsonify({"error": "An error occurred while processing your request."}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
